632b08830b784f02fa7abef13925711723e76b25
manojkumar-g8 <manojg31921@gmail.com>
2024-10-16 16:08:40 +0530

    ethu puthusu

--- src/features/posts/postAdder/PostBox.js
+++ src/features/posts/postAdder/PostBox.js
@@ -22,6 +22,9 @@ const PostBox = () => {
         }
     }, [fetchPosts, dispatch]);
 
+    // puthusu
+    const add = 0;
+
     const newArr = [...posts].reverse();
 
     const renderedPosts = newArr.map((post) => (
